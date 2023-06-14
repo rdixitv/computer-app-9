a161d204b2937eab30f9e61b1cb862af5bc0d4f2
RD <raghavd@disroot.org>
2023-06-14 17:32:56 +0530

    Minor change

--- Program016.java
+++ Program016.java
@@ -1,6 +1,6 @@
 /* Program 16
 Write a program to enter a class to the variable standard and print "high school" if the class is 8 or above and "middle school" if below 8.
-15/06/23 */
+14/06/23 */
 
 import java.util.Scanner;
 
